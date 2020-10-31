c620ce130dc01be9f7ef26f8e0840203c7d9dff8
Elvira Yolanda <elvirayolanda828@gmail.com>
2020-10-31 13:46:33 +0700

    Add project files.

---
+++ Client_ElviraYolanda_033/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_ElviraYolanda_033
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
